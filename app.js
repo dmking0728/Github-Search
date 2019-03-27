83ac58c9895395552590afd7dbe1d07582c0288f
daniel king <danielking@daniels-MacBook-Pro.local>
2019-03-26 21:30:25 -0400

    V1

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
-blank
+//initialize Github from hithub.js
+const github = new Github;
+
+//search input
+const searchUser = document.getElementById('searchUser');
+
+//search inout event listener
+searchUser.addEventListener('keyup', (e) => {
+  //get input text
+  const userText = e.target.value;
+
+  if(userText !== ''){
+    //make http call locatged in github.js
+    github.getUser(userText)
+      .then(data => {
+        if(data.profile.message === 'Not Found') {
+          //show alert
+
+        } else {
+          //show alert
+
+        }
+      })
+  } else {
+    //clear profile
+    
+  }
+});
--- github.js
+++ github.js
@@ -1,1 +1,16 @@
-blank
+class Github {
+  constructor() {
+    this.client_id = '6ba9439cd2a1bdc49baf';
+    this.client_secret = '7d0654385568fada76926ee903acf331bde5e0d8';
+  }
+  
+  async getUser(user) {
+    const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+    const profileData = await profileResponse.json();
+
+    return {
+      profile: profileData
+    }
+  }
+}
